c9ad110bca28bc16ffee465b18d6d6142dbf2ab3
emanson1 <edwardmaddenanson@gmail.com>
2023-03-21 20:31:31 -0400

    bug fixes

--- EADD.Client/src/App.jsx
+++ EADD.Client/src/App.jsx
@@ -1,119 +1,72 @@
 import React, { useState, useEffect } from 'react'
-
-import {Route, BrowserRouter as Router} from 'react-router-dom';
-// import '../src/App.css';
+import { connect } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import theme from './theme';
 import Inspection from './Pages/Inspection';
 import Installs from './Pages/Installs';
 import Inspections from './Pages/Inspections';
-import theme from './theme';
-import LoggedIn from './Pages/LoggedIn';
 import NavBar from './Pages/NavBar';
 import {View} from 'react-native';
-import ModalRoot from './Shared/ModalRoot';
-import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
-import { makeStyles} from '@material-ui/core/styles';
-import { connect } from 'react-redux';
 import {Switch as Toggle} from '@mui/material';
-import { hideModal } from './actions/eaddActions';
+import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
 
-
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-});
-
-const useStylesLight = makeStyles((lightTheme) => ({
-  paper: {
-    //position: 'absolute',
-    //width: 400,
-    backgroundColor: "black",
-    border: `'2px solid ${theme.palette.primary}'`,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
+const useStyles = makeStyles((theme) => ({
+  mainpage:
+  {
+    border:(theme.palette.type==="light")?'1px solid white':'1px solid black',
+  }
 }));
-const useStylesDark = makeStyles((darkTheme) => ({
-  paper: {
-    //position: 'absolute',
-    //width: 400,
-    backgroundColor: "white",
-    border: `'2px solid ${theme.palette.primary}'`,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
+
+const tabs = {
+  'installs': Installs,
+  'inspections': Inspections,
+
+}
 
 
 
 function App(props) {
-  const modalProps=props.modalProps;
-  const modalType=modalProps.modalType;
-  
-  const colorMode = React.useContext(ColorModeContext);
-
-  const open=modalProps.open;
-  const [selectedTab, setSelectedTab] = useState('instrument');
-  const classes = useStylesDark();
-  const [modalStyle] = React.useState(getModalStyle);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const classes=useStyles();
+  const [selectedTab, setSelectedTab] = useState('inspections');
   const [themeStyle, setThemeStyle] = useState('light');
-  const theme = useTheme();
+  const toggleTheme = () => {
+    if (themeStyle === 'light') {
+    setThemeStyle('dark');
+    } else {
+    setThemeStyle('light');
+    }
+    };
     useEffect(() => {
       document.body.className = themeStyle;
       }, [themeStyle]);
-const switchTheme = ()
-      return (
-    <ThemeProvider theme={theme}>
-      classes={JSON.stringify(classes)}
-      body={document.body.className}
-      palette={JSON.stringify(theme.palette.mode)}
       
-    <Toggle onClick={}/>Toggle Theme 
+  return (
+    <ThemeProvider theme={theme}>
+    <div className={`App ${themeStyle}`}>
+      classes={JSON.stringify(classes)}
+    <Toggle onClick={toggleTheme}/>Toggle Theme
   
-    {/* <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}> */}
-    <React.Fragment>
-      <div className={classes.paper}>
-      <Router>
-        <Route exact path='/' render={(props) => (<Inspections themeStyle={theme} {...props} />)}/>
-        <Route path='/Inspection/:id' component={Inspection} />
-      </Router>
-    
-       {modalType!==undefined && modalType!==null && <ModalRoot handleClose={hideModal} open={open}/> }
-       </div>
-       </React.Fragment>
-       {/* </View> */}
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <div>
+    <BrowserRouter>
+      <Switch>
+      <Route exact path='/' component ={Inspections} />
+     <Route path='/Inspection/:id' component={Inspection} />
+   
+    </Switch>
+    </BrowserRouter>  
+   
+    <NavBar setSelectedTab={setSelectedTab}/>
+    </div>
+       </View>
+        </div>
         </ThemeProvider>
- 
-       )
+    )
 }
 const mapStateToProps = (state) => ({
-  modalProps: state.eadd.modalProps,
-  modalType: state.eadd.modalType
-});
-const mapDispatchToProps = (dispatch) => ({
-  hideModal: () => dispatch(hideModal()),
-  
 });
 
+const mapDispatchToProps = (dispatch) => ({
+});
 export default connect(mapStateToProps, mapDispatchToProps)(App);
